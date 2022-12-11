b57d47d64bc435056db13ad25d3977409b6328f8
Jungsu-Jung <wjdwjdtn92@naver.com>
2022-12-11 17:09:58 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    let answer = 0;
+    const maxNum = Math.max(a, b);
+    const minNum = Math.min(a, b);
+    
+    for (let i = minNum; i <= maxNum; i++) {
+        answer += i;
+    }
+    
+    return answer;
+}
