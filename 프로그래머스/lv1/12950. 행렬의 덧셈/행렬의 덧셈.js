c6b10aff683434da057c15611e40de691ffeccb6
Jungsu-Jung <wjdwjdtn92@naver.com>
2022-12-12 15:11:47 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.30 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,14 @@
+function solution(arr1, arr2) {
+    var answer = [];
+
+    for (let i = 0; i < arr1.length; i++) {
+        const sum = [];
+        
+        for (let j = 0; j < arr1[0].length; j++) {
+            sum.push(arr1[i][j] + arr2[i][j]);
+        }
+        answer.push(sum);
+    }
+    
+    return answer;
+}
