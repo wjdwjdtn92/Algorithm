264e0ac12379b96a64d8bb08fcb16b826b57a55d
Jungsu-Jung <wjdwjdtn92@naver.com>
2022-12-28 21:58:03 +0900

    [level 2] Title: 짝지어 제거하기, Time: 27.45 ms, Memory: 46.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,18 @@
+function solution(s) {
+    const stack = [];
+ 
+    let i = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        if (stack.length !== 0 
+            && stack[stack.length - 1] === s[i]) {
+            stack.pop();
+            continue;
+        }
+        
+        stack.push(s[i]);
+    }
+    
+
+    return stack.length === 0 ? 1 : 0;
+}
