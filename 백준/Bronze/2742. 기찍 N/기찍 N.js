0d59157e5b69614b1eadc77016e6a087d4703cf8
Jungsu-Jung <wjdwjdtn92@naver.com>
2022-12-31 16:45:54 +0900

    [Bronze IV] Title: 기찍 N, Time: 232 ms, Memory: 22800 KB -BaekjoonHub

--- 백준/Bronze/2742. 기찍 N/기찍 N.js
+++ 백준/Bronze/2742. 기찍 N/기찍 N.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
-const input = fs.readFileSync("/dev/stdin").toString().trim();
+const input = fs.readFileSync("/dev/stdin").toString();
+const n = Number(input);
 let result = "";
 
-for (let i = Number(input); i >= 1; i--) {
-    result += `${i}\n`;
+for (let i = n; i >= 1; i--) {
+    result += i + "\n";
 }
 
 console.log(result);
