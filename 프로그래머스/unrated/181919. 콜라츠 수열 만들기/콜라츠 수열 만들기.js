4840be4ffaf663f261a1b61ff74a767661ebb6e1
Jungsu-Jung <wjdwjdtn92@naver.com>
2023-06-04 23:26:03 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    const answer = [n];
+    
+    while(n !== 1) {
+        if (n % 2 === 0) {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        
+        answer.push(n);
+    }
+    
+    return answer;
+}
